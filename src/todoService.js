436d60775b9d43e14b66fe6bf89de531e80d6cf1
Hachemi Hamadi <ham.bicho@gmail.com>
2022-11-12 16:57:38 +0100

    - add change order function

--- src/todoService.js
+++ src/todoService.js
@@ -2,6 +2,7 @@ export const todoService = {
     fetchTodos,
     createTodo,
     markTodoAsCompleted,
+    changeTodosOrder,
 };
 
 async function fetchTodos() {
@@ -92,6 +93,37 @@ async function markTodoAsCompleted(todoId) {
         return !!data.status;
     } catch (exception) {
         console.log("Exception marking todo as completed")
+        return false;
+    }
+}
+
+async function changeTodosOrder(first, second) {
+    try {
+        // Get the token
+        let token = localStorage.getItem("token");
+
+        if (!token || token.length === 0) {
+            return false;
+        }
+
+        let response = await fetch(
+            `https://factory-digital-test.herokuapp.com/api/v1/todos/updateOrder`,
+            {
+                method: 'PUT',
+                headers: {
+                    "Authorization": `Bearer ${token}`
+                },
+                body: JSON.stringify([
+                    first,
+                    second
+                ])
+            });
+        const data = await response.json();
+
+        // Check if the result is good
+        return !!data.status;
+    } catch (exception) {
+        console.log("Exception changing todo order")
         return false;
     }
 }
